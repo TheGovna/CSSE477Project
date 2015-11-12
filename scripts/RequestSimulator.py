82a383f552e2e62e6a1ba3f66e8c956c5a8a74fa
Melissa Thai <thaimp@rose-hulman.edu>
2015-11-11 23:28:54 -0500

    simulator mostly works

--- scripts/RequestSimulator.py
+++ scripts/RequestSimulator.py
@@ -1,29 +1,70 @@
 import httplib
 import random
+import json
+from pprint import pprint
 
 def simulateTraffic():
 	requestTypes = ['GET', 'PUT', 'POST', 'DELETE'];
 	server = httplib.HTTPConnection('localhost', 8023)
 	server.connect()
 	while(1):
-		# TODO: random pause
 		randomRequest = random.choice(requestTypes)
 		
 		if randomRequest == 'GET':
-			print ""
+			getOrModify = random.randint(0, 1);
+			
+			if getOrModify == 0:
+				# GET all books
+				print("Attempting GET ALL")
+				server.request('GET', '/Plugin1/books')
+
+				# if response.status == httplib.OK:
+				# 	print "GET: OK"
+			else:
+				# GET specific book
+				print("Attempting GET MODIFY")
+				randomBook = getRandomBook()
+				server.request('GET', '/Plugin1/book/' + randomBook['author'] + "/" + randomBook['title'])
+					
 		elif randomRequest == 'PUT':
-			print ""
+			randomBook = getRandomBook()
+			modifyAuthorOrTitle = random.randint(0, 1);
+			print("Attempting PUT")
+
+			if modifyAuthorOrTitle == 0:
+				# Modify author
+				randomAuthorNumber = random.randint(1, 100)
+				server.request('PUT', '/Plugin1/book/Author' + str(randomAuthorNumber) + "/" + randomBook['title'])
+			else:
+				# Modify title
+				randomTitleNumber = random.randint(1, 100)
+				server.request('PUT', '/Plugin1/book/' + randomBook['author'] + "/Title" + str(randomTitleNumber))
+
+			# if response.status == httplib.OK:
+			# 	print "PUT: OK"
+
 		elif randomRequest == 'POST':
+			print("Attempting POST")
 			randomAuthorNumber = random.randint(1, 100)
 			randomTitleNumber = random.randint(1, 100)
 			server.request('POST', '/Plugin1/book/Author' + str(randomAuthorNumber) + "/Title" + str(randomTitleNumber))
+
+			# if response.status == httplib.OK:
+			# 	print "POST: OK"
 		else:
-			print ""
+			# DELETE request
+			print("Attempting DELETE")
+			randomBook = getRandomBook()
+			server.request('DELETE', '/Plugin1/book/' + randomBook['author'] + "/" + randomBook['title'])
 
 		response = server.getresponse()
-		if response.status == httplib.OK:
-			print "got OK"
+		print randomRequest + " " + str(response.status)
 
+def getRandomBook():
+	with open('../books.json') as data_file:
+		booksJson = json.load(data_file)
+		randomBook = random.choice(booksJson)
+		return randomBook
 
 def main():
 	simulateTraffic()
